82e4e729c0a2b4573ae8f82314c64666a00a9163
Cem  Onal <mehmetcemonal@gmail.com>
2024-03-12 11:34:54 -0400

    New Project created / layout design created

---
+++ GameCards_Project/GameCards_Project/CardSelectionVC.swift
@@ -0,0 +1,26 @@
+//
+//  CardSelectionVC.swift
+//  GameCards_Project
+//
+//  Created by Student on 3/12/24.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController 
+{
+    
+    
+    
+
+    override func viewDidLoad() 
+    {
+        super.viewDidLoad()
+
+        ]
+    }
+    
+
+   
+
+}
