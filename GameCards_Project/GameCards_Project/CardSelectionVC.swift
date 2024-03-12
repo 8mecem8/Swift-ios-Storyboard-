9044338242b1088870a6cfbc739861f36af73467
Cem  Onal <mehmetcemonal@gmail.com>
2024-03-12 15:37:12 -0400

    all components added and working / app test is done

--- GameCards_Project/GameCards_Project/CardSelectionVC.swift
+++ GameCards_Project/GameCards_Project/CardSelectionVC.swift
@@ -9,18 +9,55 @@ import UIKit
 
 class CardSelectionVC: UIViewController 
 {
+    //References of UI elements
+    @IBOutlet var cardImageView: UIImageView!
+    @IBOutlet var Buttons: [UIButton]!
     
+    var cardsArray:[UIImage] = Card.allValues
+    var timer: Timer!
     
-    
-
-    override func viewDidLoad() 
+    override func viewDidLoad()
     {
         super.viewDidLoad()
-
-        ]
+        
+        //Style UI elements
+        for item in Buttons{item.layer.cornerRadius = 8}
+        
+        StartTimer()
     }
     
-
-   
-
+    
+    override func viewWillDisappear(_ animated: Bool) 
+    {
+        super.viewWillDisappear(animated)
+        
+        timer.invalidate()
+    }
+    
+    
+    func StartTimer()
+    {
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage()
+    {
+        cardImageView.image = cardsArray.randomElement() ?? UIImage(named: "AS")!
+    }
+    
+    
+    // ---- UI element`s actions
+    @IBAction func stopButtonTapped(_ sender: UIButton)
+    {
+        timer.invalidate()
+    }
+    
+    @IBAction func restartButtonTapped(_ sender: UIButton) 
+    {
+        timer.invalidate()
+        StartTimer()
+        
+    }
+    
+    
 }
